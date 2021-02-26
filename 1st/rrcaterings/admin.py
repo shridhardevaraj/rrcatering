f3eb4b1b15804eba530fed9d7c47c7f0cfeaee69
shridhardevaraj <79641893+shridhardevaraj@users.noreply.github.com>
2021-02-26 15:37:15 +0530

    Add files via upload

---
+++ 1st/rrcaterings/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(mariageT)
+admin.site.register(mariageL)
+admin.site.register(mariageS)
+admin.site.register(mariageD)
+admin.site.register(mariage2T)
+admin.site.register(mariage2L)
+admin.site.register(mariage2S)
+admin.site.register(mariage2D)
+admin.site.register(kattu)
+admin.site.register(mariage3L)
+admin.site.register(vira)
