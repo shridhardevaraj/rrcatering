47dc45907293435162711f32f21cd78be95f6ea8
shridhardevaraj <79641893+shridhardevaraj@users.noreply.github.com>
2021-02-26 12:56:00 +0530

    Add files via upload

--- 1st/rrcatering/settings.py
+++ 1st/rrcatering/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +125,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
